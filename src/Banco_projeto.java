0e9017f0b8f06d28b7c103a623ea06d8932df48f
Marcos Paulo <marcos.olisilva007@gmail.com>
2021-05-02 22:51:24 -0300

    Criação do construtor da classe e do método saldo

--- src/Banco_projeto.java
+++ src/Banco_projeto.java
@@ -1,2 +1,13 @@
 public class Banco_projeto {
+    private double saldoBanco;
+
+    public Banco_projeto(){
+        this.saldoBanco=0;
+    }
+
+    public double saldo(){
+        return saldoBanco;
+    }
+
+
 }
